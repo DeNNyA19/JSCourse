fd348c7476dd85d216b26b896ff1b58f0290f493
Denis Korbovsky <deniskorbovskii@mail.ru>
2020-01-09 12:54:51 +0300

    removed code from ws3

--- pages/exchangerate/constants.js
+++
@@ -1,7 +0,0 @@
-const dateFormat = "YYYY-MM-DD";
-const methods = {
-    latestRate: (base) => `latest?base=${base}`,
-    historyRate: (startAt, endAt, base) => `history?start_at=${startAt}&end_at=${endAt}&base=${base}` 
-}
-
-module.exports = { dateFormat, methods }
